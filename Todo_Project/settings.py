689950942ecd050446d3e13d4a5cc0b3ffc3a535
Krystian <krystian.waszczuk@infotech.edu.pl>
2024-02-22 11:24:53 +0100

    db

--- Todo_Project/settings.py
+++ Todo_Project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_app.apps.TodoAppConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'Todo_Project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
